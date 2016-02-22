0d10cd71b054798ced42e4d58e48cc1fa5b6fcbc
Florian Lippisch <florian.lippisch@netapp.com>
2016-02-22 22:34:28 +0100

    Added saving and closing to the generic editor

--- de.xwic.sandbox.demoapp.ui/src/de/xwic/sandbox/demoapp/ui/addressbook/experimental/ExperimentalEditorPage.java
+++ de.xwic.sandbox.demoapp.ui/src/de/xwic/sandbox/demoapp/ui/addressbook/experimental/ExperimentalEditorPage.java
@@ -16,19 +16,38 @@
 package de.xwic.sandbox.demoapp.ui.addressbook.experimental;
 
 import de.jwic.base.IControlContainer;
+import de.jwic.controls.ToolBar;
+import de.jwic.controls.ToolBarGroup;
+import de.jwic.controls.actions.Action;
+import de.jwic.controls.actions.IAction;
 import de.xwic.appkit.core.config.ConfigurationManager;
 import de.xwic.appkit.core.config.Profile;
 import de.xwic.appkit.core.config.editor.EditorConfiguration;
+import de.xwic.appkit.core.dao.ValidationResult;
+import de.xwic.appkit.core.model.EntityModelException;
+import de.xwic.appkit.webbase.editors.EditorContext;
 import de.xwic.appkit.webbase.editors.GenericEditorInput;
 import de.xwic.appkit.webbase.editors.GenericEntityEditor;
+import de.xwic.appkit.webbase.editors.ValidationException;
+import de.xwic.appkit.webbase.editors.mappers.MappingException;
+import de.xwic.appkit.webbase.toolkit.app.ExtendedApplication;
 import de.xwic.appkit.webbase.toolkit.app.InnerPage;
+import de.xwic.appkit.webbase.toolkit.app.Site;
 import de.xwic.appkit.webbase.toolkit.editor.EditorModel;
+import de.xwic.appkit.webbase.toolkit.util.ImageLibrary;
 
 /**
  * @author Lippisch
  *
  */
 public class ExperimentalEditorPage extends InnerPage {
+
+	private GenericEntityEditor editor;
+	private EditorContext context;
+
+	private IAction actionSave;
+	private IAction actionSaveClose;
+	private IAction actionClose;
 
 	/**
 	 * @param container
@@ -39,10 +58,105 @@ public class ExperimentalEditorPage extends InnerPage {
 		
 		setTitle("Experimental Editor");
 		
+		createActions();
+		createToolbar();
 		createContent(editorModel);
 		
 	}
 	
+	/**
+	 * 
+	 */
+	private void createActions() {
+
+		actionSave = new Action() {
+			@Override
+			public void run() {
+				performSave();
+			}
+		};
+		actionSave.setTitle("Save");
+		actionSave.setIconEnabled(ImageLibrary.ICON_SAVE_ACTIVE);
+		actionSave.setIconDisabled(ImageLibrary.ICON_SAVE_INACTIVE);
+
+		actionSaveClose = new Action() {
+			@Override
+			public void run() {
+				if (performSave()) {
+					closeEditor();
+				}
+			}
+		};
+		actionSaveClose.setTitle("Save & Close");
+		actionSaveClose.setIconEnabled(ImageLibrary.ICON_SAVECLOSE_ACTIVE);
+		actionSaveClose.setIconDisabled(ImageLibrary.ICON_SAVECLOSE_INACTIVE);
+
+		actionClose = new Action() {
+			@Override
+			public void run() {
+				closeEditor();
+			}
+		};
+		actionClose.setTitle("Close");
+		actionClose.setIconEnabled(ImageLibrary.ICON_CLOSED);
+		actionClose.setIconDisabled(ImageLibrary.ICON_CLOSED_INACTIVE);
+
+	}
+
+	/**
+	 * 
+	 */
+	protected void closeEditor() {
+		
+		Site site = ExtendedApplication.getInstance(this).getSite();
+		site.popPage(this);
+		
+	}
+
+	/**
+	 * 
+	 */
+	protected boolean performSave() {
+		try {
+			ValidationResult validationResult = context.saveToEntity();
+			if (validationResult.hasErrors()) {
+				getSessionContext().notifyMessage("There have been issues... *eek*", "error");
+			} else {
+				getSessionContext().notifyMessage("Your changes have been saved...");
+				return true;
+			}
+		} catch (ValidationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (MappingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (EntityModelException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return false;
+	}
+
+	/**
+	 * 
+	 */
+	private void createToolbar() {
+		
+		ToolBar toolbar = new ToolBar(this, "toolbar");
+		ToolBarGroup grpDefault = toolbar.addGroup();
+
+		grpDefault.addAction(actionClose);
+		grpDefault.addAction(actionSaveClose);
+		grpDefault.addAction(actionSave);
+		
+		
+	}
+
+	/**
+	 * Create the editor.
+	 * @param editorModel
+	 */
 	private void createContent(EditorModel editorModel) {
 		
 		try {
@@ -50,7 +164,8 @@ public class ExperimentalEditorPage extends InnerPage {
 			EditorConfiguration editorConfig = profile.getEditorConfiguration(editorModel.getEntity().type().getName());
 			
 			GenericEditorInput input = new GenericEditorInput(editorModel.getEntity(), editorConfig);
-			GenericEntityEditor editor = new GenericEntityEditor(this, "editor", input);
+			editor = new GenericEntityEditor(this, "editor", input);
+			context = editor.getContext();
 		
 		} catch (Exception e) {
 			log.error("Error creating editor", e);
