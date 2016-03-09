c0da4363014809ca4b5e09e0b6aa7d1aceae96fc
vitaliy <vzhovtiuk@gmail.com>
2016-03-09 11:39:32 +0100

    campaign participant entity selector support

--- de.xwic.sandbox.demoapp.ui/src/de/xwic/sandbox/demoapp/ui/DemoAppModule.java
+++ de.xwic.sandbox.demoapp.ui/src/de/xwic/sandbox/demoapp/ui/DemoAppModule.java
@@ -23,6 +23,7 @@ import de.xwic.appkit.webbase.toolkit.app.Site;
 import de.xwic.appkit.webbase.toolkit.app.SubModule;
 import de.xwic.sandbox.base.model.SandboxModelConfig;
 import de.xwic.sandbox.base.model.util.ConfigurationUtil;
+import de.xwic.sandbox.demoapp.ui.addressbook.CampaignParticipantSubmodule;
 import de.xwic.sandbox.demoapp.ui.addressbook.CampaignSubmodule;
 import de.xwic.sandbox.demoapp.ui.addressbook.CompanySubmodule;
 import de.xwic.sandbox.demoapp.ui.addressbook.ContactsSubmodule;
@@ -51,6 +52,7 @@ public class DemoAppModule extends Module {
 		if (ConfigurationUtil.hasAccess(SandboxModelConfig.SMOD_DEMO_APP_ADDRESS_BOOK)) {		
 			subModules.add(new CompanySubmodule(site));
 			subModules.add(new CampaignSubmodule(site));
+			subModules.add(new CampaignParticipantSubmodule(site));
 			subModules.add(new ContactsSubmodule(site));
 		}
 
