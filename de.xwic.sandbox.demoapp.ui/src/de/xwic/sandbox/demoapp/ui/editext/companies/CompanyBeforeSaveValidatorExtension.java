9b2433f54d91af2180bb7ed4c1655a105c936a86
vitaliy <vzhovtiuk@gmail.com>
2016-04-20 09:46:08 +0200

    company editor validation and editor extensions

--- de.xwic.sandbox.demoapp.ui/src/de/xwic/sandbox/demoapp/ui/editext/companies/CompanyBeforeSaveValidatorExtension.java
+++ de.xwic.sandbox.demoapp.ui/src/de/xwic/sandbox/demoapp/ui/editext/companies/CompanyBeforeSaveValidatorExtension.java
@@ -5,7 +5,8 @@ package de.xwic.sandbox.demoapp.ui.editext.companies;
 
 import de.xwic.appkit.core.dao.ValidationResult;
 import de.xwic.appkit.core.model.IEntityModel;
-import de.xwic.appkit.webbase.editors.IEditorValidator;
+import de.xwic.appkit.webbase.editors.IEditorValidatorListener;
+import de.xwic.appkit.webbase.editors.events.ValidationEvent;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
@@ -16,7 +17,7 @@ import org.apache.commons.logging.LogFactory;
  *
  * @author Vitaliy Zhovtyuk
  */
-public class CompanyBeforeSaveValidatorExtension implements IEditorValidator {
+public class CompanyBeforeSaveValidatorExtension implements IEditorValidatorListener {
 
     protected final static Log log = LogFactory.getLog(CompanyBeforeSaveValidatorExtension.class);
 
@@ -27,17 +28,21 @@ public class CompanyBeforeSaveValidatorExtension implements IEditorValidator {
     }
 
     @Override
-    public void validate(IEntityModel model, ValidationResult validationResult) {
-        String website;
-        try {
-            website = String.valueOf(model.getProperty("webSite"));
-        } catch (Exception e) {
-            log.error("Failed to read property " + e.getMessage(), e);
-            throw new RuntimeException(e.getMessage(), e);
-        }
-        if (!website.startsWith("http")) {
-            validationResult.addError("de.xwic.sandbox.demoapp.model.entities.ICompany.webSite",
-                    "de.xwic.sandbox.demoapp.model.entities.ICompany.webSite.httpStart");
+    public void modelValidated(ValidationEvent validationEvent) {
+        final IEntityModel model = (IEntityModel) validationEvent.getEventSource();
+        final ValidationResult validationResult = validationEvent.getValidationResult();
+        if(validationEvent.isOnSave()) {
+            final String website;
+            try {
+                website = String.valueOf(model.getProperty("webSite"));
+            } catch (Exception e) {
+                log.error("Failed to read property " + e.getMessage(), e);
+                throw new RuntimeException(e.getMessage(), e);
+            }
+            if (!website.startsWith("http")) {
+                validationResult.addError("de.xwic.sandbox.demoapp.model.entities.ICompany.webSite",
+                        "de.xwic.sandbox.demoapp.model.entities.ICompany.webSite.httpStart");
+            }
         }
     }
 }
